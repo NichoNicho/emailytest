a7c8369b1843b1916dd54efd435a676f1f37f4c2
mattias zworner <mattias.zworner@hiq.se>
2018-04-05 12:39:29 +0200

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+// production keys here!!!
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+
+googleClientID: '447649181427-ulskilv6ju2u4asq4os8ulgcbs8k9n5o.apps.googleusercontent.com',
+googleClientSecret: 'DZ-G3vATFEO8GJsnuwwrDlzx',
+mongoURI: 'mongodb://nicholas:WeAre100Sexy@ds235169.mlab.com:35169/emaily-prod'
+cookieKey: 'oisibduyfbvksdviuuqwyrporopjcjbxchvsdghfvihwsvbtffhsbkjfeb'
