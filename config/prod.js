8f9537b0340ea230de109eb2ae145c6c7b94aed8
mattias zworner <mattias.zworner@hiq.se>
2018-04-05 12:45:22 +0200

    fixed heroku issue

--- config/prod.js
+++ config/prod.js
@@ -5,9 +5,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-
-
-googleClientID: '447649181427-ulskilv6ju2u4asq4os8ulgcbs8k9n5o.apps.googleusercontent.com',
-googleClientSecret: 'DZ-G3vATFEO8GJsnuwwrDlzx',
-mongoURI: 'mongodb://nicholas:WeAre100Sexy@ds235169.mlab.com:35169/emaily-prod'
-cookieKey: 'oisibduyfbvksdviuuqwyrporopjcjbxchvsdghfvihwsvbtffhsbkjfeb'
